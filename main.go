e94218de724021eb8b28923587fc52cff2483a0f
Daniil Belov <danbelov@gmx.ch>
2020-11-03 13:56:35 +0100

    Correction to a previous commit

--- main.go
+++ main.go
@@ -18,13 +18,13 @@ var errInvalidArguments = errors.New("Invalid arguments")
 var errReadingInput = errors.New("Error reading input")
 
 func main() {
-	ou := strings.ToUpper(os.Args[1])
+	originUnit := strings.ToUpper(os.Args[1])
 	if len(os.Args) > 2 {
 		printError(errInvalidArguments)
 	}
 
 	for {
-		fmt.Print("What is the current temperature in " + ou + " ? ")
+		fmt.Print("What is the current temperature in " + originUnit + " ? ")
 
 		fmt.Print("Would you like to convert another temperature ? (y/n) ")
 
